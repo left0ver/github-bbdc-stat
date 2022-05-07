11f1ca68d0d5b90a087346ca2c4f512a9f1d49c0
leftover <1614674987@qq.com>
2022-05-07 21:58:18 +0800

    配置根路径404

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const  bbdcRouter = require('./router/bbdc')
+const {Error404}= require('./error_pages')
 const app = express();
+app.use('/',(req, res) => {
+    res.status(404).send(new Error404().render())
+})
 app.use(bbdcRouter)
 app.listen(process.env.PORT || 8000, () => {
     console.log('srart');
