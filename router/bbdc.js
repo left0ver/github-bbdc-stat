e844c4d1bbaf80c5daafe1e57c20b60635f7a2ea
Lithium <81900789+lichium@users.noreply.github.com>
2023-08-26 20:54:36 +0800

    Update bbdc.js
    增加显示当日学习情况

--- router/bbdc.js
+++ router/bbdc.js
@@ -49,18 +49,26 @@ transform="translate(8,6)"
 <rect width="364" height="172" fill="${renderColor[theme].BACKGROUND}"  rx="10" ry="10"/>
 </g>
  <text x="97"  y="44"  fill="${renderColor[theme].TITLE}" font-size="19" font-weight="600" font-family="SegoeUI, Segoe UI">${data.nickname}'s  bbdc  Stats</text>
- <text x="10"  y="90"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Learn num</text>
- <text x="19"  y="130" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalLearn} words</text>
- <text x="120" y="90"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Review num</text>
- <text x="135" y="130" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalReview} words</text>
- <text x="235" y="90"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Duration time</text>
- <text x="260" y="130" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalDuration} mins</text>
+ <text x="15"  y="115"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Today</text>
+ <text x="15"  y="150"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Week</text>
+
+ <text x="95"  y="80"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Learn</text>
+ <text x="85"  y="115" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.todayLearn} words</text>
+ <text x="85"  y="150" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalLearn} words</text>
+
+ <text x="185" y="80"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Review</text>
+ <text x="180" y="115" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.todayReview} words</text>
+ <text x="180" y="150" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalReview} words</text>
+ 
+ <text x="280" y="80"  fill="${renderColor[theme].TEXT}"  font-size="15" font-weight="600" font-family="SegoeUI, Segoe UI">Duration</text>
+ <text x="285" y="115" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.msg}</text>
+ <text x="285" y="150" fill="${renderColor[theme].TEXT}"  font-size="13" font-weight="600" font-family="SegoeUI, Segoe UI">${data.totalDuration} mins</text>
 </svg>
 `
 }
 
 bbdcRouter.get('/bbdc', async (req, res) => {
-    const { userId, theme, nickname, hide_border, title_color, text_color } = req.query
+    const { userId, theme, nickname, hide_border, title_color, text_color , msg } = req.query
     // 如果没有userId，返回404
     if (userId === undefined) {
         return res.status(400).send(new Error400('没有userId').render())
@@ -68,6 +76,8 @@ bbdcRouter.get('/bbdc', async (req, res) => {
     let totalDuration = 0
     let totalLearn = 0
     let totalReview = 0
+    let todayLearn = 0
+    let todayReview = 0
     const { data } = await get(`profile/search`, {
         baseURL,
         params: { userId }
@@ -76,13 +86,16 @@ bbdcRouter.get('/bbdc', async (req, res) => {
     if (data.result_code === 20000) {
         return res.status(400).send(new Error400('userId不正确').render())
     }
-    const { learnList, durationList } = data.data_body
-    for (let i = 0, len = learnList.length; i < len; i++) {
-        totalDuration += durationList[i].duration
-        totalLearn += learnList[i].learnNum
-        totalReview += learnList[i].reviewNum
+    const { learnList } = data.data_body
+    let len = learnList.length
+    totalDuration = data.data_body.total_duration_num //总时长//原时常计算方法大概率失效
+    todayLearn = data.data_body.learnList[len-1]["learnNum"]
+    todayReview = data.data_body.learnList[len-1]["reviewNum"]
+    for (let i = 0; i < len; i++) {
+        totalLearn += learnList[i].learnNum //近一周学习数
+        totalReview += learnList[i].reviewNum //近一周复习数
     }
     res.header("Content-Type", "image/svg+xml",)
-    res.send(render(COLORS, handleTheme(COLORS, theme), { totalDuration, totalLearn, totalReview, nickname: nickname === undefined ? 'leftover' : nickname, hide_border, title_color, text_color }))
+    res.send(render(COLORS, handleTheme(COLORS, theme), { msg : msg === undefined ? '(≧∇≦)ﾉ✧' : msg, todayLearn, todayReview, totalDuration, totalLearn, totalReview, nickname: nickname === undefined ? 'leftover' : nickname, hide_border, title_color, text_color }))
 })
 module.exports = bbdcRouter
